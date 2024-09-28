44694808cb9bb21c2cd747f36ef49fe55475c21e
KiranKalluri888 <saikirankalluri888.com>
2024-09-28 10:07:24 +0530

    Day9 commit

---
+++ Day9/Expjs/server.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+
+app.get("/getUser",(req,res)=>{
+  res.send("Welcome to getUser endpoint");
+})
+
+app.listen(5500,()=>{
+    console.log("My server is on port 5500");
+})
